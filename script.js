2d4ffe6b92621473f35aba5c7e7530a5c08b9cef
Vipul Singh <120732585+Vipul1305@users.noreply.github.com>
2023-06-14 05:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 	(sampleObject[key] === undefined) ? false : true;
-}
+
 }
 
 // Do not change the code below
  const key = prompt("Enter Key.");
 alert(hasKey(key));
+
